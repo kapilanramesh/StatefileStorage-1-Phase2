21c5e053b0ffbf168d669ee4255de1bb27ee2c92
kapilan <166731523+kapilanramesh@users.noreply.github.com>
2025-04-08 17:08:17 +0530

    variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0f58b397bc5c1f2e8" # Ubuntu 22.04 LTS in ap-south-1
+}
